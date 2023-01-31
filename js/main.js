c0dec9788f954986a91e3f292543eed466ecb1a8
r3r3d <123917940+r3r3d@users.noreply.github.com>
2023-01-31 23:45:19 +0700

    2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        inventory: 12,
+        inventory: 0,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -44,18 +44,10 @@ let app = new Vue({
                 variantSize: 'XXL',
             }],
         cart: 0,
-        methods: {
-            addToCart() {
-                this.cart += 1
-            },
-            updateProduct(variantImage) {
-                this.image = variantImage
-            },
-            removeFromCart() {
-                this.cart -= 1
-            }
-        }
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+        },
 
-        }
 
 })
