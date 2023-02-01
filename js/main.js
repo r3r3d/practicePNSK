7d6e29cc1875d2b3ea85098d191fb8a3599a26bf
gr413_rered <mrstream378@gmail.com>
2023-02-01 09:38:12 +0700

    1

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
-        inventory: 0,
+        brand:'Vue Mastery ',
+        inventory: 11,
         onSale: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
@@ -14,40 +14,56 @@ let app = new Vue({
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 10
             }
         ],
 
         sizes: [
             {
                 sizeId: 2236,
-                variantSize: 'S',
-            },
-            {
-                sizeId: 2237,
-                variantSize: 'M',
-            },
-            {
-                sizeId: 2238,
-                variantSize: 'L',
-            },
-            {
-                sizeId: 2239,
-                variantSize: 'XL',
-            },
-            {
-                sizeId: 2240,
-                variantSize: 'XXL',
+                variantSize: 'S, M, L, XL, XXL'
             }],
+
         cart: 0,
-        updateProduct(variantImage) {
-            this.image = variantImage
-        },
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
 
+            selectedVariant: 0,
+        },
+        computed:{
+        title() {
+            return this.brand + '' + this.product;
+        },
+        image() {
+                return this.variants[this.selectedVariant].variantImage;
+        },
+            inStock(){
+                return this.variants[this.selectedVariant].variantQuantity
+            }
 
-})
+        },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        removeFromCart(){
+            this.cart -= 1
+        }
+
+    },
+
+
+
+
+
+
+
+    })
